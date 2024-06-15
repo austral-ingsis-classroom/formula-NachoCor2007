8d2ef5a18aa35b2b9cbe4ed2ff2660e162cb7dcc
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-06-15 13:36:04 -0300

    Had to modify build.gradle so that the project could work. Started re-implementation of tests.

--- src/test/java/edu/austral/ingsis/math/ListVariablesTest.java
+++ src/test/java/edu/austral/ingsis/math/ListVariablesTest.java
@@ -1,76 +1,79 @@
 package edu.austral.ingsis.math;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.empty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.austral.ingsis.math.operands.Constant;
+import edu.austral.ingsis.math.operators.Sum;
+import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
 
 public class ListVariablesTest {
 
   /** Case 1 + 6 */
   @Test
   public void shouldListVariablesFunction1() {
-    final List<String> result = Collections.emptyList();
+    Function sum = new Sum(new Constant(1), new Constant(6));
+    MathResolver mathResolver = new MathResolver(sum);
 
-    assertThat(result, empty());
+    final List<String> result = mathResolver.getVariables();
+
+    assertEquals(result, Collections.emptyList());
   }
 
-  /** Case 12 / div */
-  @Test
-  public void shouldListVariablesFunction2() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("div"));
-  }
-
-  /** Case (9 / x) * y */
-  @Test
-  public void shouldListVariablesFunction3() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("x", "y"));
-  }
-
-  /** Case (27 / a) ^ b */
-  @Test
-  public void shouldListVariablesFunction4() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("a", "b"));
-  }
-
-  /** Case z ^ (1/2) */
-  @Test
-  public void shouldListVariablesFunction5() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("z"));
-  }
-
-  /** Case |value| - 8 */
-  @Test
-  public void shouldListVariablesFunction6() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("value"));
-  }
-
-  /** Case |value| - 8 */
-  @Test
-  public void shouldListVariablesFunction7() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("value"));
-  }
-
-  /** Case (5 - i) * 8 */
-  @Test
-  public void shouldListVariablesFunction8() {
-    final List<String> result = Collections.emptyList();
-
-    assertThat(result, containsInAnyOrder("i"));
-  }
+//  /** Case 12 / div */
+//  @Test
+//  public void shouldListVariablesFunction2() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertEquals(result, containsInAnyOrder("div"));
+//  }
+//
+//  /** Case (9 / x) * y */
+//  @Test
+//  public void shouldListVariablesFunction3() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("x", "y"));
+//  }
+//
+//  /** Case (27 / a) ^ b */
+//  @Test
+//  public void shouldListVariablesFunction4() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("a", "b"));
+//  }
+//
+//  /** Case z ^ (1/2) */
+//  @Test
+//  public void shouldListVariablesFunction5() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("z"));
+//  }
+//
+//  /** Case |value| - 8 */
+//  @Test
+//  public void shouldListVariablesFunction6() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("value"));
+//  }
+//
+//  /** Case |value| - 8 */
+//  @Test
+//  public void shouldListVariablesFunction7() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("value"));
+//  }
+//
+//  /** Case (5 - i) * 8 */
+//  @Test
+//  public void shouldListVariablesFunction8() {
+//    final List<String> result = Collections.emptyList();
+//
+//    assertThat(result, containsInAnyOrder("i"));
+//  }
 }
