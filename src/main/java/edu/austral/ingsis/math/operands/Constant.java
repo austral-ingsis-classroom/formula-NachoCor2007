60c31e0462b3ba9be12757b91ead43340a707c17
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-06-15 13:33:14 -0300

    Implemented a Sum operator and a Constant operand that implement the Function interface.

---
+++ src/main/java/edu/austral/ingsis/math/operands/Constant.java
@@ -0,0 +1,34 @@
+package edu.austral.ingsis.math.operands;
+
+import java.math.BigDecimal;
+import edu.austral.ingsis.math.Function;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class Constant implements Function {
+  private final double value;
+
+  public Constant(double value) {
+    this.value = value;
+  }
+
+  @Override
+  public Optional<Double> resolve(Map<String, Double> variables) {
+    return Optional.of(this.value);
+  }
+
+  @Override
+  public Set<String> getVariables() {
+    return Set.of();
+  }
+
+  @Override
+  public String toString() {
+    return trimZeroes(this.value);
+  }
+
+  private String trimZeroes(Double number) {
+      return BigDecimal.valueOf(number).stripTrailingZeros().toPlainString();
+  }
+}
